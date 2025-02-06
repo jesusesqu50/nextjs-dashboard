9942c8fb6e34fad7df49761cc98b2649b5e19763
jesusesqu50 <L20650184@zitacuaro.tecnm.mx>
2025-02-06 09:19:47 -0600

    Actualización del proyecto terminado

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,5 +1,5 @@
-import bcrypt from 'bcrypt';
 import postgres from 'postgres';
+import bcryptjs from 'bcryptjs';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -17,7 +17,7 @@ async function seedUsers() {
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
-      const hashedPassword = await bcrypt.hash(user.password, 10);
+      const hashedPassword = await bcryptjs.hash(user.password, 10);
       return sql`
         INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
